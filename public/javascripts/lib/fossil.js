fd567f1ea0ad3bc48ccd97a6a11a821cede7fc70
Michael Ball <mwball@gmail.com>
2011-10-22 11:29:31 -0400

    update fossil info

--- public/javascripts/lib/fossil.js
+++ public/javascripts/lib/fossil.js
@@ -1,82 +1,52 @@
 (function() {
   window.specimens = {
-    DunbarellaPELECYPOD: {
-      name: "Dunbarella PELECYPOD",
-      photo_image: "test_photo_image",
-      drawing_image: "test_drawing_image",
-      description: "This is a test description"
+    PELECYPOD: {
+      name: "PELECYPOD",
+      photo_image: "Pelecypod_pic_1.jpg",
+      drawing_image: "Pelecepod_anat.jpg",
+      description: "<b>Pelecypod</b> is an ancestor to modern clams, oysters, scallops, and mussels. These are all bibivalved, meaning they have a shell composed of two hinged parts, or valves, held together by internal muscles. Pelecypods relax their hinge muscles to open their shells. The shell of the Pelecypod is made of aragonite calcium carbonate. This differs from the Brachiopod's mineral calcium carbonate and made the Pelecypod shell less likely to be preserved. Pelecypods attached themselves to the ocean floor by means of an internal foot. They also had fringing tentacles for gathering food."
     },
-    GlycimerisPELECYPOD: {
-      name: "Glycimeris PELECYPOD",
-      photo_image: "test_photo_image",
-      drawing_image: "test_drawing_image",
-      description: "This is a test description"
+    BRACHIOPOD: {
+      name: "BRACHIOPOD",
+      photo_image: ["Brachiopod_glyptorthis_pic.jpg", "Brachiopod_holtedahlina_pic.jpg", "Brachiopod_sowerbyella_pic.jpg", "Brachiopod_strpohomena_pic_1.jpg", "Brachiopod_strpohomena_pic_2.jpg", "Brachiopod_strpohomena_pic_3.jpg", "Brachiopod_trematis_pic.jpg", "Brachiopod_zygospira_modesta_pic.jpg"],
+      drawing_image: "Brachiopod_anat.jpg",
+      description: "<b>Brachiopods</b> are relatively simple organisms. They have a soft body enclosed in two shells that can be opened for feeding.  Each brachiopod valve (the top and bottom shell halves) is bilaterally symmetrical from the center of the hinge to the point of the shell opening opposite to the hinge. That is, each brachiopod valve can be divided in two from hinge to front, and the halves are mirror images of each other. However the top and bottom valves are not mirror images of each other. It has a feeding apparatus, called a lothophore, that strains food particles from the water and channels them to a mouth and digestive tract. There is also a weak heart-like organ that pumps blood through the body cavity. However, the brachiopod takes up most of its oxygen directly from the surrounding sea, particularly through the lothophore. The brachiopod is distinguished by an organ that is used to anchor it to the ocean floor. It is called the 'pedicle' and is a fleshy, muscular stock or 'foot'. The pedicle could be inserted into the bottom mud and secured with the aid of a mucus-like secretion."
     },
-    LimaPELECYPOD: {
-      name: "Lima PELECYPOD",
-      photo_image: "test_photo_image",
-      drawing_image: "test_drawing_image",
-      description: "This is a test description"
+    TRILOBYTE: {
+      name: "TRILOBYTE",
+      photo_image: ["Trilobite_pic_1.gif", "Trilobite_pic_2.jpg"],
+      drawing_image: "Trilobite_anat.jpg",
+      description: "<b>Trilobites</b> are extinct members of the Arthropod family which were quite common. Fossils of these creatures, although plentiful, are sometimes difficult to find.  They are most often found 'enrolled' (rolled in a ball). This may have been a way of defending themselves from predators (such as the cephalopod) or from severe storms that often ravaged the Ordovician Sea. Many died in this position and were found 440 million years later as fossils. One possible reason why they are more commonly found enrolled than outstretched is that they hold together as fossils better than ones that are buried in prone positions. The body and appendages of a Trilobite are encased in and exoskeleton made of protein chitin. The exoskeleton is like a suit of armor with rigid parts articulated by flexible joints. Trilobites are classified as arthropods, which literally means 'jointed legs.' Arthropods grow by periodically shedding their exoskeletons and forming new skin. These shed exoskeletons, called molts, are sometimes the fossil remains and may partly explain the abundance of trilobite fossils. There are 16 genera of Trilobites from the Cincinnati series. They were thought to have actively hunted prey buried in the sediment, based on their leg structure and chewing mouth parts."
     },
-    ZygospiraBRACHIOPOD: {
-      name: "Zygospira BRACHIOPOD",
-      photo_image: "test_photo_image",
-      drawing_image: "test_drawing_image",
-      description: "This is a test description"
+    BRYOZOAN: {
+      name: "BRYOZOAN",
+      photo_image: ["Bryozoan_pic.jpg"],
+      drawing_image: ["Bryozoan_anat_1.jpg", "Bryozoan_anat_2.jpg"],
+      description: "<b>Bryozoans</b>, or 'moss-animals' are colonial animals that secreted a hard skeleton similar to that of corals. The colony consists of thousands of individual animals, each living within their own tube, called an aperture. The living aperatures are of a size that a sewing needle might make. Sometimes the outer surface is covered with distinctive bumps or ridges (monticules). Colony shapes vary from delicate open mesh fronds to branching forms, massive mounds, and heavy dense fronds. Sometimes one colony will show multiple forms. Bryozoans are active filter feeders. They feed on minute organisms and bits of organic matter suspended in the water. Each little animal in the colony spreads the tentacles of its lophoshere into a funnel-like shape and the cilia that line the tentacles move in such a way that food particles are carried down to the mouth. When the members of the colony do this at the same time, it creates a current in the water that enhances feeding opportunities for the collective."
     },
-    SchuchertellaBRACHIOPOD: {
-      name: "Schuchertella BRACHIOPOD",
-      photo_image: "test_photo_image",
-      drawing_image: "test_drawing_image",
-      description: "This is a test description"
+    CRINOID: {
+      name: "CRINOID",
+      photo_image: "Crinoid_pic.jpg",
+      drawing_image: ["Crinoid_anat_1.jpg", "Crinoid_anat_2.jpg"],
+      description: "<b>Crinoids</b> or, 'sea lilies' are in the same taxonomic group as modern starfish , sand dollars, and sea cucumbers. Crinoids have persisted from the Ordovician period to the present, although the specific group found in these rocks disappeared at the end of the Permian period. Crinoids lived attached to the bottom of the sea by a long, flexible column and a root-like base. This column is the most common type of fossilized remain found from crinoids. The main part of the body, the calyx, at the upper end of the column, had arms that channeled food into the mouth on the upper surface of the calyx. Fossils showing the calyx and arms are rare. The hard structure of the animal was composed of plates of crystalline calcium carbonate."
     },
-    PlatystrophiaBRACHIOPOD: {
-      name: "Platystrophia BRACHIOPOD",
-      photo_image: "test_photo_image",
-      drawing_image: "test_drawing_image",
-      description: "This is a test description"
-    },
-    SpiriferBRACHIOPOD: {
-      name: "Spirifer BRACHIOPOD",
-      photo_image: "test_photo_image",
-      drawing_image: "test_drawing_image",
-      description: "This is a test description"
-    },
-    FlexicalymeneTRILOBYTE: {
-      name: "Flexicalymene TRILOBYTE",
-      photo_image: "test_photo_image",
-      drawing_image: "test_drawing_image",
-      description: "This is a test description"
-    },
-    RhomboporaBRYOZOAN: {
-      name: "Rhombopora BRYOZOAN",
-      photo_image: "test_photo_image",
-      drawing_image: "test_drawing_image",
-      description: "This is a test description"
-    },
-    TaxocrinusCRINOID: {
-      name: "Taxocrinus CRINOID",
-      photo_image: "test_photo_image",
-      drawing_image: "test_drawing_image",
-      description: "This is a test description"
-    },
-    DolorthoscerasCEPHALOPOD: {
-      name: "Dolorthosceras CEPHALOPOD",
-      photo_image: "test_photo_image",
-      drawing_image: "test_drawing_image",
-      description: "This is a test description"
+    CEPHALOPOD: {
+      name: "CEPHALOPOD",
+      photo_image: "cephalopod_pic.jpg",
+      drawing_image: "Cephalopod_anat.jpg",
+      description: "<b>Cephalopod</b> or, 'head foot' looked as though its feet (actually tentacles) were growing out of its head. It was a distant relative of squids and octopus. Some grew as large as twelve feet in length. They were likely capable of rapid movement and may have had powerful grasping tentacles, strong beaks, good eyesight and strong shells. They were very likely the largest and most numerous predators of their time."
     },
     GASTROPOD: {
       name: "GASTROPOD",
-      photo_image: "test_photo_image",
-      drawing_image: "test_drawing_image",
-      description: "This is a test description"
+      photo_image: ["Gastropod1.jpg", "Gastropod2.jpg"],
+      drawing_image: ["Gastropod_anat_1.jpg", "Gastropod_anat_2.jpg"],
+      description: "Gastropod or, 'stomach foot' was the ancestor to the modern snail. After insects, snails  have the highest number of different species among all animals. Since the early Cambrian period, they have slowly but continuously increased in variety and importance up to present day. Certain forms have adapted to terrestrial life, but must are aquatic."
     },
-    StreptelasmaHORNCORAL: {
-      name: "Streptelasma HORN CORAL",
-      photo_image: "test_photo_image",
-      drawing_image: "test_drawing_image",
-      description: "This is a test description"
+    HORNCORAL: {
+      name: "HORN CORAL",
+      photo_image: "HornCoral_pic.jpg",
+      drawing_image: ["HornCoral_anat_1.jpg", "HornCoral_anat_2.jpg"],
+      description: "<b>Corals</b> are found today in great abundance and variety in marine rocks, and have existed as far back as the Cambrian period. 'Horn coral' or 'cup coral' resembles the modern sea-anemones but had a thick calcium carbonate skeleton (corallite), formed from its secretions. Horn Corals are part of a group of animals called Cnidaria (stinging tentacles). They had hollow, sack-like tentacles they used to grab food from the surrounding seawater. The fossil is only the corallite of the organism -- a skeleton without the flesh."
     }
   };
   window.fossils_data = [
@@ -86,96 +56,58 @@
       next_statement_pair: 2
     }, {
       key_pair: 1,
-      statement: "The fossil does not resembles a modern day seashell",
-      next_statement_pair: 8
-    }, {
-      key_pair: 2,
-      statement: "The top side of the fossil is rounded with ribs",
+      statement: "The fossil does not resemble a modern day seashell",
       next_statement_pair: 3
     }, {
       key_pair: 2,
-      statement: "The top and bottom of the fossil have ribs and ridges",
-      next_statement_pair: 5
+      help: "Visually divide the shell in half across the top from the back hindge to front opening.",
+      statement: "The right and left sides of the shell are mirror images of each other.",
+      specimen: specimens.BRACHIOPOD
+    }, {
+      key_pair: 2,
+      help: "Visually divide the shell in half across the top from the back hindge to front opening.",
+      statement: " The right and left sides of the shell are not mirror images of each other.",
+      specimen: specimens.PELECYPOD
     }, {
       key_pair: 3,
-      statement: "The fossil has a circle or oval outline shape",
+      statement: "The fossil is long and tubular (tube shaped) or conical (cone shaped).",
       next_statement_pair: 4
     }, {
       key_pair: 3,
-      statement: "The fossil has a flat semi-circle shape",
-      specimen: specimens.DunbarellaPELECYPOD
+      statement: "The fossil is 'bug shaped', ribbed with three sections, lip and eyes.",
+      specimen: specimens.TRILOBYTE
     }, {
       key_pair: 4,
-      statement: "The fossil has a circular outline with light ribs",
-      specimen: specimens.GlycimerisPELECYPOD
+      statement: "The fossil is tubular.",
+      next_statement_pair: 5
     }, {
       key_pair: 4,
-      statement: "The fossil has an oval outline with a very pointed pedicle",
-      specimen: specimens.LimaPELECYPOD
+      statement: "The fossil is conical.",
+      next_statement_pair: 7
     }, {
       key_pair: 5,
-      statement: "The fossil is larger than pinky fingernail",
+      statement: "The fossil has rounded sections.",
       next_statement_pair: 6
     }, {
       key_pair: 5,
-      statement: "The fossil is approximate pinky fingernail size",
-      specimen: specimens.ZygospiraBRACHIOPOD
+      statement: "The fossil is branching with pin holes.",
+      specimen: specimens.BRYOZOAN
     }, {
       key_pair: 6,
-      statement: "The fossil has very distinct ribs",
-      next_statement_pair: 7
+      statement: "The fossil is thin like a pencil and has holes on cross-section.",
+      specimen: specimens.CRINOID
     }, {
       key_pair: 6,
-      statement: "The fossil has faint ribs with a predominant central ridge",
-      specimen: specimens.SchuchertellaBRACHIOPOD
+      statement: "The fossil is wide with thick elongating ribs.",
+      specimen: specimens.CEPHALOPOD
     }, {
       key_pair: 7,
-      statement: "The fossil has large ribs and very slight ridge",
-      specimen: specimens.PlatystrophiaBRACHIOPOD
-    }, {
-      key_pair: 7,
-      statement: "The fossil has predominant ridge and valley",
-      specimen: specimens.SpiriferBRACHIOPOD
-    }, {
-      key_pair: 8,
-      statement: "The fossil is long and tubular (tube shape) or conical (cone shape)",
-      next_statement_pair: 9
-    }, {
-      key_pair: 8,
-      statement: "The fossil is ribbed with three sections and a lip",
-      specimen: specimens.FlexicalymeneTRILOBYTE
-    }, {
-      key_pair: 9,
-      statement: "The fossil is tubular",
-      next_statement_pair: 10
-    }, {
-      key_pair: 9,
-      statement: "The fossil is conical",
-      next_statement_pair: 12
-    }, {
-      key_pair: 10,
-      statement: "The fossil has round sections",
-      next_statement_pair: 11
-    }, {
-      key_pair: 10,
-      statement: "The fossil is branching with pin holes",
-      specimen: specimens.RhomboporaBRYOZOAN
-    }, {
-      key_pair: 11,
-      statement: "The fossil is thin like a pencil and has holes on cross-section",
-      specimen: specimens.TaxocrinusCRINOID
-    }, {
-      key_pair: 11,
-      statement: "The fossil is wide with elongating ribs",
-      specimen: specimens.DolorthoscerasCEPHALOPOD
-    }, {
-      key_pair: 12,
-      statement: "The fossil has a spiral shape",
+      statement: "The fossil has a spiral shape.",
       specimen: specimens.GASTROPOD
     }, {
-      key_pair: 12,
-      statement: "The fossil resembles a dinosaur tooth with radial sections",
-      specimen: specimens.StreptelasmaHORNCORAL
+      key_pair: 7,
+      statement: "The fossil resembles a dinosaur tooth with radial sections.",
+      specimen: specimens.HORNCORAL
     }
   ];
 }).call(this);
