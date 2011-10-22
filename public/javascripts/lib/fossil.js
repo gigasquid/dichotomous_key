2a2597884933206f20a65318a98cbcb2aba40b1b
gigasquid <cmeier@gigasquidsoftware.com>
2011-10-22 13:34:51 -0400

    fix short description syntax error and update image data to match pic names

--- public/javascripts/lib/fossil.js
+++ public/javascripts/lib/fossil.js
@@ -10,66 +10,74 @@
       name: "BRACHIOPOD",
       photo_image: ["Brachiopod_glyptorthis_pic.jpg", "Brachiopod_holtedahlina_pic.jpg", "Brachiopod_sowerbyella_pic.jpg", "Brachiopod_strpohomena_pic_1.jpg", "Brachiopod_strpohomena_pic_2.jpg", "Brachiopod_strpohomena_pic_3.jpg", "Brachiopod_trematis_pic.jpg", "Brachiopod_zygospira_modesta_pic.jpg"],
       drawing_image: "Brachiopod_anat.jpg",
-      description: "<b>Brachiopods</b> are relatively simple organisms. They have a soft body enclosed in two shells that can be opened for feeding.  Each brachiopod valve (the top and bottom shell halves) is bilaterally symmetrical from the center of the hinge to the point of the shell opening opposite to the hinge. That is, each brachiopod valve can be divided in two from hinge to front, and the halves are mirror images of each other. However the top and bottom valves are not mirror images of each other. It has a feeding apparatus, called a lothophore, that strains food particles from the water and channels them to a mouth and digestive tract. There is also a weak heart-like organ that pumps blood through the body cavity. However, the brachiopod takes up most of its oxygen directly from the surrounding sea, particularly through the lothophore. The brachiopod is distinguished by an organ that is used to anchor it to the ocean floor. It is called the 'pedicle' and is a fleshy, muscular stock or 'foot'. The pedicle could be inserted into the bottom mud and secured with the aid of a mucus-like secretion."
+      description: "<b>Brachiopods</b> are relatively simple organisms. They have a soft body enclosed in two shells that can be opened for feeding.  Each brachiopod valve (the top and bottom shell halves) is bilaterally symmetrical from the center of the hinge to the point of the shell opening opposite to the hinge. That is, each brachiopod valve can be divided in two from hinge to front, and the halves are mirror images of each other. However the top and bottom valves are not mirror images of each other. It has a feeding apparatus, called a lothophore, that strains food particles from the water and channels them to a mouth and digestive tract. There is also a weak heart-like organ that pumps blood through the body cavity. However, the brachiopod takes up most of its oxygen directly from the surrounding sea, particularly through the lothophore. The brachiopod is distinguished by an organ that is used to anchor it to the ocean floor. It is called the 'pedicle' and is a fleshy, muscular stock or 'foot'. The pedicle could be inserted into the bottom mud and secured with the aid of a mucus-like secretion.",
+      short_description: "Brachiopods are relatively simple organisms. They have a soft body enclosed in two shells that can be opened for feeding.  Pelecypod is an ancestor to modern clams, oysters, scallops, and mussels. These are all bibivalved, meaning they have a shell composed of two hinged parts, or valves."
     },
     TRILOBYTE: {
       name: "TRILOBYTE",
       photo_image: ["Trilobite_pic_1.gif", "Trilobite_pic_2.jpg"],
       drawing_image: "Trilobite_anat.jpg",
-      description: "<b>Trilobites</b> are extinct members of the Arthropod family which were quite common. Fossils of these creatures, although plentiful, are sometimes difficult to find.  They are most often found 'enrolled' (rolled in a ball). This may have been a way of defending themselves from predators (such as the cephalopod) or from severe storms that often ravaged the Ordovician Sea. Many died in this position and were found 440 million years later as fossils. One possible reason why they are more commonly found enrolled than outstretched is that they hold together as fossils better than ones that are buried in prone positions. The body and appendages of a Trilobite are encased in and exoskeleton made of protein chitin. The exoskeleton is like a suit of armor with rigid parts articulated by flexible joints. Trilobites are classified as arthropods, which literally means 'jointed legs.' Arthropods grow by periodically shedding their exoskeletons and forming new skin. These shed exoskeletons, called molts, are sometimes the fossil remains and may partly explain the abundance of trilobite fossils. There are 16 genera of Trilobites from the Cincinnati series. They were thought to have actively hunted prey buried in the sediment, based on their leg structure and chewing mouth parts."
+      description: "<b>Trilobites</b> are extinct members of the Arthropod family which were quite common. Fossils of these creatures, although plentiful, are sometimes difficult to find.  They are most often found 'enrolled' (rolled in a ball). This may have been a way of defending themselves from predators (such as the cephalopod) or from severe storms that often ravaged the Ordovician Sea. Many died in this position and were found 440 million years later as fossils. One possible reason why they are more commonly found enrolled than outstretched is that they hold together as fossils better than ones that are buried in prone positions. The body and appendages of a Trilobite are encased in and exoskeleton made of protein chitin. The exoskeleton is like a suit of armor with rigid parts articulated by flexible joints. Trilobites are classified as arthropods, which literally means 'jointed legs.' Arthropods grow by periodically shedding their exoskeletons and forming new skin. These shed exoskeletons, called molts, are sometimes the fossil remains and may partly explain the abundance of trilobite fossils. There are 16 genera of Trilobites from the Cincinnati series. They were thought to have actively hunted prey buried in the sediment, based on their leg structure and chewing mouth parts.",
+      short_description: "Trilobites are extinct members of the Arthropod family. The body and appendages of a Trilobite are encased in and exoskeleton made of protein chitin."
     },
     BRYOZOAN: {
       name: "BRYOZOAN",
       photo_image: ["Bryozoan_pic.jpg"],
       drawing_image: ["Bryozoan_anat_1.jpg", "Bryozoan_anat_2.jpg"],
-      description: "<b>Bryozoans</b>, or 'moss-animals' are colonial animals that secreted a hard skeleton similar to that of corals. The colony consists of thousands of individual animals, each living within their own tube, called an aperture. The living aperatures are of a size that a sewing needle might make. Sometimes the outer surface is covered with distinctive bumps or ridges (monticules). Colony shapes vary from delicate open mesh fronds to branching forms, massive mounds, and heavy dense fronds. Sometimes one colony will show multiple forms. Bryozoans are active filter feeders. They feed on minute organisms and bits of organic matter suspended in the water. Each little animal in the colony spreads the tentacles of its lophoshere into a funnel-like shape and the cilia that line the tentacles move in such a way that food particles are carried down to the mouth. When the members of the colony do this at the same time, it creates a current in the water that enhances feeding opportunities for the collective."
+      description: "<b>Bryozoans</b>, or 'moss-animals' are colonial animals that secreted a hard skeleton similar to that of corals. The colony consists of thousands of individual animals, each living within their own tube, called an aperture. The living aperatures are of a size that a sewing needle might make. Sometimes the outer surface is covered with distinctive bumps or ridges (monticules). Colony shapes vary from delicate open mesh fronds to branching forms, massive mounds, and heavy dense fronds. Sometimes one colony will show multiple forms. Bryozoans are active filter feeders. They feed on minute organisms and bits of organic matter suspended in the water. Each little animal in the colony spreads the tentacles of its lophoshere into a funnel-like shape and the cilia that line the tentacles move in such a way that food particles are carried down to the mouth. When the members of the colony do this at the same time, it creates a current in the water that enhances feeding opportunities for the collective.",
+      short_description: "Bryozoans, or 'moss-animals' are colonial animals that secreted a hard skeleton similar to that of corals. The colony consists of thousands of individual animals, each living within their own tube, called an aperture."
     },
     CRINOID: {
       name: "CRINOID",
       photo_image: "Crinoid_pic.jpg",
       drawing_image: ["Crinoid_anat_1.jpg", "Crinoid_anat_2.jpg"],
-      description: "<b>Crinoids</b> or, 'sea lilies' are in the same taxonomic group as modern starfish , sand dollars, and sea cucumbers. Crinoids have persisted from the Ordovician period to the present, although the specific group found in these rocks disappeared at the end of the Permian period. Crinoids lived attached to the bottom of the sea by a long, flexible column and a root-like base. This column is the most common type of fossilized remain found from crinoids. The main part of the body, the calyx, at the upper end of the column, had arms that channeled food into the mouth on the upper surface of the calyx. Fossils showing the calyx and arms are rare. The hard structure of the animal was composed of plates of crystalline calcium carbonate."
+      description: "<b>Crinoids</b> or, 'sea lilies' are in the same taxonomic group as modern starfish , sand dollars, and sea cucumbers. Crinoids have persisted from the Ordovician period to the present, although the specific group found in these rocks disappeared at the end of the Permian period. Crinoids lived attached to the bottom of the sea by a long, flexible column and a root-like base. This column is the most common type of fossilized remain found from crinoids. The main part of the body, the calyx, at the upper end of the column, had arms that channeled food into the mouth on the upper surface of the calyx. Fossils showing the calyx and arms are rare. The hard structure of the animal was composed of plates of crystalline calcium carbonate.",
+      short_description: "Crinoids or, 'sea lilies' are in the same taxonomic group as modern starfish , sand dollars, and sea cucumbers. Crinoids have persisted from the Ordovician period to the present."
     },
     CEPHALOPOD: {
       name: "CEPHALOPOD",
       photo_image: "cephalopod_pic.jpg",
       drawing_image: "Cephalopod_anat.jpg",
-      description: "<b>Cephalopod</b> or, 'head foot' looked as though its feet (actually tentacles) were growing out of its head. It was a distant relative of squids and octopus. Some grew as large as twelve feet in length. They were likely capable of rapid movement and may have had powerful grasping tentacles, strong beaks, good eyesight and strong shells. They were very likely the largest and most numerous predators of their time."
+      description: "<b>Cephalopod</b> or, 'head foot' looked as though its feet (actually tentacles) were growing out of its head. It was a distant relative of squids and octopus. Some grew as large as twelve feet in length. They were likely capable of rapid movement and may have had powerful grasping tentacles, strong beaks, good eyesight and strong shells. They were very likely the largest and most numerous predators of their time.",
+      short_description: "Cephalopod or, 'head foot' looked as though its feet (actually tentacles) were growing out of its head. It was a distant relative of squids and octopus."
     },
     GASTROPOD: {
       name: "GASTROPOD",
       photo_image: ["Gastropod1.jpg", "Gastropod2.jpg"],
       drawing_image: ["Gastropod_anat_1.jpg", "Gastropod_anat_2.jpg"],
-      description: "Gastropod or, 'stomach foot' was the ancestor to the modern snail. After insects, snails  have the highest number of different species among all animals. Since the early Cambrian period, they have slowly but continuously increased in variety and importance up to present day. Certain forms have adapted to terrestrial life, but must are aquatic."
+      description: "Gastropod or, 'stomach foot' was the ancestor to the modern snail. After insects, snails  have the highest number of different species among all animals. Since the early Cambrian period, they have slowly but continuously increased in variety and importance up to present day. Certain forms have adapted to terrestrial life, but must are aquatic.",
+      short_description: "Gastropod or, 'stomach foot' was the ancestor to the modern snail. After insects, snails  have the highest number of different species among all animals."
     },
     HORNCORAL: {
       name: "HORN CORAL",
       photo_image: "HornCoral_pic.jpg",
       drawing_image: ["HornCoral_anat_1.jpg", "HornCoral_anat_2.jpg"],
-      description: "<b>Corals</b> are found today in great abundance and variety in marine rocks, and have existed as far back as the Cambrian period. 'Horn coral' or 'cup coral' resembles the modern sea-anemones but had a thick calcium carbonate skeleton (corallite), formed from its secretions. Horn Corals are part of a group of animals called Cnidaria (stinging tentacles). They had hollow, sack-like tentacles they used to grab food from the surrounding seawater. The fossil is only the corallite of the organism -- a skeleton without the flesh."
+      description: "<b>Corals</b> are found today in great abundance and variety in marine rocks, and have existed as far back as the Cambrian period. 'Horn coral' or 'cup coral' resembles the modern sea-anemones but had a thick calcium carbonate skeleton (corallite), formed from its secretions. Horn Corals are part of a group of animals called Cnidaria (stinging tentacles). They had hollow, sack-like tentacles they used to grab food from the surrounding seawater. The fossil is only the corallite of the organism -- a skeleton without the flesh.",
+      short_description: "Horn Corals are part of a group of animals called Cnidaria (stinging tentacles). They had hollow, sack-like tentacles they used to grab food from the surrounding seawater."
     }
   };
   window.fossils_data = [
     {
       key_pair: 1,
       statement: "The fossil resembles a modern day seashell",
-      help_image: "1a_seashell.png",
+      help_image: "1a_seashell_1.png",
       next_statement_pair: 2
     }, {
       key_pair: 1,
       statement: "The fossil does not resemble a modern day seashell",
+      help_image: "1a_seashell_2.png",
       next_statement_pair: 3
     }, {
       key_pair: 2,
       help: "Visually divide the shell in half across the top from the back hindge to front opening.",
       statement: "The right and left sides of the shell are mirror images of each other.",
-      help_image: "2a_mirrored.png",
+      help_image: "2a_mirrored_1.png",
       specimen: specimens.BRACHIOPOD
     }, {
       key_pair: 2,
       help: "Visually divide the shell in half across the top from the back hindge to front opening.",
       statement: " The right and left sides of the shell are not mirror images of each other.",
-      help_image: "2b_not_mirrored.png",
+      help_image: "2b_mirrored_2.png",
       specimen: specimens.PELECYPOD
     }, {
       key_pair: 3,
@@ -79,7 +87,7 @@
     }, {
       key_pair: 3,
       statement: "The fossil is 'bug shaped', ribbed with three sections, lip and eyes.",
-      help_image: "3b_bug_shape.png",
+      help_image: "3b_bug_shape_2.gif",
       specimen: specimens.TRILOBYTE
     }, {
       key_pair: 4,
